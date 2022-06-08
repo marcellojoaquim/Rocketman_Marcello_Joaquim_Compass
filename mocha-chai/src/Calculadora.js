e10dde7391b7cb13d314c472d17e8a792a6d341e
Marcello Joaquim da Silva <marcellojoaquim1@hotmail.com>
2022-06-08 17:22:23 -0300

    continuacao mocha-chai

---
+++ mocha-chai/src/Calculadora.js
@@ -0,0 +1,48 @@
+export default class Calculadora{
+    static soma(a,b){
+        return a+b
+    }
+    static subtracao(a,b){
+        return a-b
+    }
+    static divisao(a,b){
+        return a/b
+    }
+    static multiplicacao(a,b){
+        return a*b
+    }
+    static potencia(a,b){
+        if(b==0){
+            return 1
+        }else{
+            return a * this.potencia(a,b-1);
+        }
+    }
+
+    static fatorial(a){
+        if(a==0){
+            return 1;
+        }else{
+            return a*this.fatorial(a-1);
+        }
+    }
+
+    static mdc(a,b){
+        if((b<=a)&&(a%b==0)){
+            return b
+        }else if(a <b){
+            return this.mdc(b,a);
+        }else{
+            return this.mdc(b,a%b);
+        }
+    }
+
+    static somatorio(a){
+        if(a==1){
+            return 1;
+        }else{
+            return a + this.somatorio(a-1)
+        }
+    }
+
+}
--- mocha-chai/test/sample.spec.js
+++ mocha-chai/test/sample.spec.js
@@ -1,27 +1,82 @@
 import assert from 'assert'
+import chai, { expect } from 'chai'
+import Calculadora from '../src/Calculadora.js'
 
-describe('Meu primeiro teste', () => {
-    it('Verificar igualdade', () =>{
-        let aux = 2+1;
-        assert.strictEqual(aux, 3)
+
+const axpect = chai.expect
+
+// describe('Meu primeiro teste', () => {
+//     it('Verificar igualdade1', () =>{
+//         let aux = 2+1;
+// //        assert.strictEqual(aux, 3)
+//         expect(aux).to.be.equals(3);
+//     })
+
+//     it('Verificar igualdade2', () =>{
+//         let aux = 3;
+// //        assert.strictEqual(aux, 3)
+//         expect(aux).to.be.equals(3);
+//     })
+
+// })
+
+// describe('Meu segundo teste', () => {
+//     it('Verificar igualdade', () =>{
+//         let aux = 'skull';
+//         assert.strictEqual(aux, 'head')
+//     })
+
+//     it('Verificar negacao de igualdade', () =>{
+//         let aux = 3;
+//         assert.notStrictEqual(aux, '3')
+//     })
+
+// })
+
+describe('Teste Calculadora', () => {
+    it('Verificar Soma', () =>{
+        let resultado = Calculadora.soma(1, 2);
+        expect(resultado).to.be.eq(3);
+    })
+    it('Verificar Soma de números negativos', () =>{
+        let resultado = Calculadora.soma(-1, 2);
+        expect(resultado).to.be.eq(1);
     })
 
-    it('Verificar igualdade', () =>{
-        let aux = 3;
-        assert.strictEqual(aux, 3)
+    it('Verificar Subtração', () =>{
+        let resultado = Calculadora.subtracao(1, 1);
+        expect(resultado).to.be.eq(0);
+    })
+    it('Verificar Multiplicação', () =>{
+        let resultado = Calculadora.multiplicacao(1, 1);
+        expect(resultado).to.be.eq(1);
     })
 
-})
-
-describe('Meu segundo teste', () => {
-    it('Verificar igualdade', () =>{
-        let aux = 'skull';
-        assert.strictEqual(aux, 'head')
+    it('Verificar Divisão', () =>{
+        let resultado = Calculadora.divisao(2, 1);
+        expect(resultado).to.be.eq(2);
     })
 
-    it('Verificar negacao de igualdade', () =>{
-        let aux = 3;
-        assert.notStrictEqual(aux, '3')
+    it('Verificar Potencia', () =>{
+        let resultado = Calculadora.potencia(2, 2);
+        expect(resultado).to.be.eq(4);
+    })
+    it('Verificar Fatorial', () =>{
+        let resultado = Calculadora.fatorial(5);
+        expect(resultado).to.be.eq(120);
     })
 
-})
+    it('Verificar MDC', () =>{
+        let resultado = Calculadora.mdc(18, 60);
+        expect(resultado).to.be.eq(6);
+    })
+
+    it('Verificar Somatorio', () =>{
+        let resultado = Calculadora.somatorio(4);
+        expect(resultado).to.be.eq(10);
+    })
+
+    })
+
+    
+
